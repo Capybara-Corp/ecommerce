6f0c128c13f58c18b78a1153e81fd51cd567ac6a
Nicolás Filipovich <filiporovich777@gmail.com>
2022-07-15 20:19:02 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,12 @@
-<?php	
-echo "funciona";
-?>
+<!DOCKTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Welcome!</title>
+    </head>
+    <body>
+        <h1>Please Login or SignUp</h1>
+        <a href="login.php">Login</a> or
+        <a href="signup.php">Sign Up</a>
+    </body>
+</html>
