a0c866d252d71df59295ce0b3b51cda1a14ee24d
Nicolás Filipovich <filiporovich777@gmail.com>
2022-07-15 22:11:05 -0300

    Update index.php

--- index.php
+++ index.php
@@ -3,13 +3,13 @@
     <head>
         <meta charset="utf-8">
         <title>Welcome!</title>
+        <link rel="stylesheet" href="assets/css/style.css">
         <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:wght@600&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="css/style.css">
     </head>
     <body>
-        <h1>Please Login or SignUp</h1>
+        <h1>Please Login or Sign Up</h1>
         <a href="login.php">Login</a> or
         <a href="signup.php">Sign Up</a>
     </body>
