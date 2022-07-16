0bddaa88e7232b5a381743e68e98c36a7a4ae67d
Nicolás Filipovich <filiporovich777@gmail.com>
2022-07-15 21:31:36 -0300

    import tipografía con serifa

--- index.php
+++ index.php
@@ -3,6 +3,10 @@
     <head>
         <meta charset="utf-8">
         <title>Welcome!</title>
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:wght@600&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="css/style.css">
     </head>
     <body>
         <h1>Please Login or SignUp</h1>
