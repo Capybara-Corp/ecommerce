56bf1caa16d8c4a507479cfa3ee8f60ed5242cbe
Santiago <elsantoposada@gmail.com>
2022-07-16 18:21:43 -0300

    Estilos añadidos

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Welcome!</title>
+        <link rel="stylesheet" href="assets/css/style.css">
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:wght@600&display=swap" rel="stylesheet">
+    </head>
+    <body> 
+    <?php require 'partials/header.php' ?>
+    <div id="todo">
+        <h1>Please: </h1>
+        <a href="login.php" id="login">Login</a> 
+        <p>or</p>
+        <a href="signup.php" id="signup">Sign Up</a>
+    </div>
+    </body>
+</html>
---
+++ login.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Login</title>
+        <link rel="stylesheet" href="assets/css/style.css">
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:wght@600&display=swap" rel="stylesheet">
+    <body>
+    <?php require 'partials/header.php' ?>
+        <h1>Login</h1>
+        <span>or <a href="signup.php">Sign Up</a></span>
+        <!-- Login Form, action es a dónde se van a enviar todos los datos, el metodo post
+        evita filtrar la información a través de la url.-->
+        <form action="login.php" method="post"></form>
+        <input type="text" name="email" placeholder="Ingresa tu correo">
+        <input type="password" name="password" placeholder="Ingresa tu contraseña">
+        <input type="submit" value="Login">
+    </body>
+</html>
