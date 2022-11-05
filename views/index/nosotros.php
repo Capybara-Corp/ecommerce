354a9d9c8b15f75f0abed188f8ae7af153e8d394
Santiago <elsantoposada@gmail.com>
2022-11-05 10:40:42 -0300

    Footer

--- views/index/nosotros.php
+++ views/index/nosotros.php
@@ -31,6 +31,7 @@ Translate::init($idioma, "lang/".$idioma.".php");
   	<meta name="viewport" content="width=device-width, initial-scale=1.0">
   	<link rel="stylesheet" href="public/css/index/nosotros.css">
 	  <link rel="stylesheet" href="public/css/index/header.css">
+	  <link rel="stylesheet" href="public/css/index/footer.css">
   	<link rel="preconnect" href="https://fonts.googleapis.com">
 	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 	<link href="https://fonts.googleapis.com/css2?family=Roboto+Condensed:wght@400;700&family=Roboto:wght@100;300;400&display=swap" rel="stylesheet">
