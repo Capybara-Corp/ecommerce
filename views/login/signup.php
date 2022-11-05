ff6d23154d7e844532a608f16bfe0f53716ba24e
Santiago <elsantoposada@gmail.com>
2022-11-05 13:36:17 -0300

    Register responsive

--- views/login/signup.php
+++ views/login/signup.php
@@ -61,6 +61,7 @@ Translate::init($idioma, "lang/".$idioma.".php");
 <html>
   <head>
   <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width,initial-scale=1">
   <title><?=Translate::__('registrarse');?></title>
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
